aa50495f6ae3ac2911b13ee20dc0ca8ad591cb86
TaeWook <>
2023-06-17 20:11:46 +0900

    Programmers oracle coding_test update

--- Programmers_test/Lv4.sql
+++ Programmers_test/Lv4.sql
@@ -200,3 +200,20 @@ ORDER BY A.amount*B.price DESC, product_id
 ;
 
 
+--Lv4 식품분류별 가장 비싼 식품의 정보 조회하기
+SELECT 
+      category
+    , price	 AS max_price
+    , product_name
+  FROM food_product
+ WHERE (category, price) IN(
+    SELECT category, MAX(price)
+        FROM food_product
+    GROUP BY category
+)
+   AND
+    category IN('과자', '국', '김치', '식용유')
+ORDER BY 2 DESC
+;
+
+
