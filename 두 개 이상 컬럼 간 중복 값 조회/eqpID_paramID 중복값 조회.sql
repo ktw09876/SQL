917a70e1e2cfe613a5ab9c99326e3dbd2211e5da
TaeWook <>
2023-06-18 12:58:10 +0900

    eqpID_paramID 중복값 조회 SQL

--- 두 개 이상 컬럼 간 중복 값 조회/eqpID_paramID 중복값 조회.sql
+++ 두 개 이상 컬럼 간 중복 값 조회/eqpID_paramID 중복값 조회.sql
@@ -1,18 +1,18 @@
 SELECT eqp_id, param_id, tag_name, tag_desc, full_address, param_code,
-        CASE 
+        CASE --
             WHEN eid_pid > 1 -- eqp_id, param_id 가 같은 경우
             THEN CASE
-                    WHEN eid_pid_desc > 1 -- tag_desc 가 같은 경우
+                    WHEN eid_pid_desc > 1 -- eqp_id, param_id, desc. 가 같은 경우
                     THEN CASE
-                            WHEN eid_pid_desc_addr > 1 -- addr 가 같은 경우
+                            WHEN eid_pid_desc_addr > 1 --eqp_id, param_id, desc., full_address가 같은 경우
                             THEN 'FULL_ADDRESS 가 같은 경우 = 태그 삭제'
-                            WHEN eid_pid_desc_addr = 1  -- addr 가 다른 경우
+                            WHEN eid_pid_desc_addr = 1  --eqp_id, param_id, desc.가 같고 full_address가 다른 경우
                             THEN 'param_code 수정'
                             ELSE '확인 필요'
                          END
-                    WHEN eid_pid_desc = 1 -- tag_desc 가 다른 경우
+                    WHEN eid_pid_desc = 1 --eqp_id, param_id가 같고 desc.가 다른 경우
                     THEN CASE 
-                            WHEN eid_pid_pcode > 1 -- desc 는 다르고 pcode 가 같은 경우
+                            WHEN eid_pid_pcode > 1 --eqp_id, param_id, param_code가 같은데 desc.는 다른 경우
                             THEN '로직 확인 건'
                             ELSE '확인 필요'
                           END
@@ -20,16 +20,15 @@ SELECT eqp_id, param_id, tag_name, tag_desc, full_address, param_code,
                   END
             ELSE '확인 필요'
          END AS msg
-  FROM (
+  FROM ( --DESC.에 비정형화 되어있는 데이터는 replace로 같은 값으로 변환
     SELECT A.*
-            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id ) AS eid_pid
-            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER')) AS eid_pid_desc
-            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER'),full_address) AS eid_pid_desc_addr
-            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, param_code) AS eid_pid_pcode
-            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER'), param_code) AS eid_pid_desc_pcode
+            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id ) AS eid_pid --eqp_id, param_id가 같은 경우
+            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER')) AS eid_pid_desc --eqp_id, param_id, desc.가 같은 경우
+            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER'),full_address) AS eid_pid_desc_addr --eqp_id, param_id, desc., full_address가 같은 경우
+            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, param_code) AS eid_pid_pcode --eqp_id, param_id, param_code가 같은 경우
+            ,COUNT(1) OVER(PARTITION BY eqp_id, param_id, REPLACE(regexp_replace(tag_desc, 'PWR|파워', 'POWER'), '오버', 'OVER'), param_code) AS eid_pid_desc_pcode --eqp_id, param_id, desc., param_code가 같은 경우
       FROM tt_mapping A
 ) A
  WHERE 
     eid_pid > 1 -- eqp_id 와 param_id 가 2개 이상인 대상만 조회
 ;
-
