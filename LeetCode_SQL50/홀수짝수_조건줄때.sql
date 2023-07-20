2cb5673f103e5d47c1acfdcf53a104b88778ac0c
ktw09876 <>
2023-07-20 20:22:34 +0900

    홀수나 짝수를 조건절에 줄 때

---
+++ LeetCode_SQL50/홀수짝수_조건줄때.sql
@@ -0,0 +1,15 @@
+/*
+오라클에서 홀수나 짝수 데이터를 조건으로 주고 싶을 때
+흔히 아는 나머지 연산자(%)는 오라클에서 지원하지 않음
+    Ex)WHERE ID % 2 = 1(X)
+    
+MOD()를 이용하자 
+    Ex)MOD(ID, 2) = 1
+*/
+SELECT *
+  FROM Cinema
+ WHERE
+       MOD(ID, 2) = 1
+   AND description <> 'boring'
+ORDER BY 1 DESC
+;
