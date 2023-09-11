d3b47ecd73893b0291dc0684ebfa6ae1c3c2aa12
ktw09876 <>
2023-09-11 10:49:42 +0900

    정규표현식 Weather Observation Station 9 모음으로 시작하지 않는 대상

---
+++ Solve SQL_HackerRank/정규표현식 Weather Observation Station 9 모음으로 시작하지 않는 대상.sql
@@ -0,0 +1,7 @@
+-- 정규표현식 Weather Observation Station 9 모음으로 시작하지 않는 대상
+select distinct
+      city
+  from station
+ where
+    not regexp_like(city, '^[a|e|i|o|u]', 'i') -- ^: 시작 문자열, 여러개인 경우 []로 묶고 |를 사용, i: 대소문자를 구분하지 않는 옵션
+;
