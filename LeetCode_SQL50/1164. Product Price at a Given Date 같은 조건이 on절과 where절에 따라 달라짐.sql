872470cc6554f40f727b445080edacd3f8e8b920
ktw09876 <>
2023-09-15 10:51:44 +0900

    1164. Product Price at a Given Date 같은 조건이 on절과 where절에 따라 달라짐

--- LeetCode_SQL50/1164. Product Price at a Given Date 같은 조건이 on절과 where절에 따라 달라짐.sql
+++ LeetCode_SQL50/1164. Product Price at a Given Date 같은 조건이 on절과 where절에 따라 달라짐.sql
@@ -20,3 +20,24 @@ SELECT DISTINCT
   )p2  ON p1.product_id=p2.product_id AND rk=1 
 --  where
 --     rk=1 
+
+-- rk = 1인 대상을 필터링 한 테이블을 left join 나라면 이렇게 했을 것
+SELECT DISTINCT
+      p1.product_id
+    , NVL(p2.new_price,10) AS price 
+  FROM 
+    products p1 
+    LEFT JOIN (
+      SELECT *
+        FROM (
+            SELECT  
+                  product_id
+                , new_price
+                , change_date
+                , DENSE_RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC ) AS rk
+              FROM products
+             WHERE change_date<=TO_DATE('2019-08-16','yyyy-mm-dd')
+        )
+       WHERE
+          rk=1
+  )p2  ON p1.product_id=p2.product_id
